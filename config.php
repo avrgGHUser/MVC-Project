0bd1cd9079c63e895059af0d30b7e696311caa19
abrgGHUser <hfeet8327@gmail.com>
2022-03-16 11:37:14 +0500

    TEST

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-const DB_HOSTNAME = '89.108.83.110';
-const DB_USERNAME = 'is342_eremin';
-const DB_PASSWORD = 'testingmysql';
-const DB_DATABASE = 'is342_eremin';
+const DB_HOSTNAME = 'mysqlis.radiotech.su';
+const DB_USERNAME = 'is342_15_admin';
+const DB_PASSWORD = 'G9of4TBpax';
+const DB_DATABASE = 'is342_15_test';
 const DB_PORT = '3306'; # Default 3306
